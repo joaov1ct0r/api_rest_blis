06fb376e5ad02e4ea10d404a858c52bf45cfd40e
joaov1ct0r <jov9051@outlook.com>
2025-10-15 18:34:19 -0300

    feat: created schema user_documents

--- start.sh
+++ start.sh
@@ -3,9 +3,16 @@
 echo "Removing old containers... \n"
 docker compose down
 docker system prune --all --volumes --force
+echo "OK! \n"
 
 echo "Starting new containers... \n"
 docker compose up -d
+echo "OK! \n"
+
+echo "Checking for missing migrations... \n"
+npx prisma migrate dev
+echo "OK! \n"
 
 echo "Starting API... \n"
 npx ts-node src/server.ts
+echo "OK! \n"
