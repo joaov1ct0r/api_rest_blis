4482af20e8c338d02472ac9708227cbd3dc7fbd1
joaov1ct0r <jov9051@outlook.com>
2025-10-15 19:15:36 -0300

    feat: created app

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import express, { Express } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import errorHandler from '@middlewares/error-handler';
+
+const app: Express = express();
+
+app.set('trust proxy', true);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: 'localhost', credentials: true }));
+app.use(helmet());
+
+app.use(errorHandler);
+
+export { app };
