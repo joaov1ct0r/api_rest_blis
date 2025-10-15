ff5c6290efb7aa3769c644c1d4a8a05d01be165c
joaov1ct0r <jov9051@outlook.com>
2025-10-15 20:20:07 -0300

    feat: created find user by email repository

---
+++ src/users/repositories/find-user-by-email-repository.ts
@@ -0,0 +1,19 @@
+import { PrismaProvider } from '@src/utils/prisma-provider';
+import { Users } from 'generated/prisma';
+
+export interface IFindUserByEmailRepository {
+  execute(email: string): Promise<Users | null>;
+}
+
+export class FindUserByEmailRepository
+  extends PrismaProvider
+  implements IFindUserByEmailRepository
+{
+  public async execute(email: string): Promise<Users | null> {
+    const user: Users | null = await this.prisma.users.findFirst({
+      where: { email },
+    });
+
+    return user;
+  }
+}
