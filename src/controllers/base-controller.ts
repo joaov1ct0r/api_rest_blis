35d1eb44adb366b99c36e1d8fa56d7ab7fa3efc3
joaov1ct0r <jov9051@outlook.com>
2025-10-16 19:11:10 -0300

    feat: added new errors to base controller

--- src/controllers/base-controller.ts
+++ src/controllers/base-controller.ts
@@ -1,7 +1,17 @@
 import { BadRequestError } from '@src/errors/bad-request-error';
+import { ForbiddenError } from '@src/errors/forbidden-error';
+import { UnauthorizedError } from '@src/errors/unauthorized-error';
 
 export abstract class BaseController {
   protected badRequest(message: string): BadRequestError {
     return new BadRequestError(message);
+  }
+
+  protected forbidden(message: string): ForbiddenError {
+    return new ForbiddenError(message);
+  }
+
+  protected unauthorized(message: string): UnauthorizedError {
+    return new UnauthorizedError(message);
   }
 }
