6a945855668a70ae51783fea1ed2067c3db22085
joaov1ct0r <jov9051@outlook.com>
2025-10-16 04:59:51 -0300

    feat: added auth files to users route files

--- src/users/routes/user-routes.ts
+++ src/users/routes/user-routes.ts
@@ -1,6 +1,7 @@
 import express, { type Router } from 'express';
 import { ZodValidation } from '@utils/zod-validation';
-import { createUserController } from '@users/singletons/index';
+import { authController } from '@auth/factories/index';
+import { createUserController } from '@users/factories/index';
 
 export const userRouter: Router = express.Router();
 
@@ -10,6 +11,10 @@ userRouter.post(
   createUserController.execute.bind(createUserController),
 );
 
-userRouter.post('/login', () => {});
+userRouter.post(
+  '/login',
+  ZodValidation.authUser,
+  authController.execute.bind(authController),
+);
 
 userRouter.post('/documents', () => {});
