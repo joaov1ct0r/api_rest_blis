bc3d9648aa3905af5f7317d0ec9477303295cb18
joaov1ct0r <jov9051@outlook.com>
2025-10-16 01:18:16 -0300

    feat: added swagger to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express, { Express } from 'express';
-import { env } from '@utils/env-config';
 import cors from 'cors';
 import helmet from 'helmet';
-import { ErrorHandler } from '@middlewares/error-handler';
+import { env } from '@utils/env-config';
+import express, { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from '@utils/swagger-config';
 import { userRouter } from '@users/routes/user-routes';
+import { ErrorHandler } from '@middlewares/error-handler';
+import swaggerDocument from '../swagger.json';
 
 const app: Express = express();
 
@@ -15,6 +18,12 @@ app.use(cors({ origin: env.HOST, credentials: true }));
 app.use(helmet());
 
 app.use('/users', userRouter);
+
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerSpec),
+);
 
 app.use(ErrorHandler.execute);
 
--- src/users/schemas/create-user-body-schema.ts
+++ src/users/schemas/create-user-body-schema.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
 
-export const createUserBodySchema = z.object({
+const createUserBodySchema = z.object({
   name: z.string({ error: 'Nome é obrigatório!' }),
   birthdate: z.string({ error: 'Data de nascimento é obrigatória!' }),
   email: z.email({ error: 'Email é obrigatório!' }),
   password: z.string({ error: 'Senha é obrigatória!' }),
 });
+
+type ICreateUserBodySchema = z.infer<typeof createUserBodySchema>;
+
+export { createUserBodySchema, ICreateUserBodySchema };
