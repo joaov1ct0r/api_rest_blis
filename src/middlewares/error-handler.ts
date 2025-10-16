42b497d0760dc35f612589c4f3c575bc86007529
joaov1ct0r <jov9051@outlook.com>
2025-10-15 23:35:46 -0300

    feat: created middleware error handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,16 +1,7 @@
-import type { ErrorRequestHandler, RequestHandler } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import type { Request, Response } from 'express';
 
-const unexpectedRequest: RequestHandler = (_req, res) => {
-  res.status(StatusCodes.NOT_FOUND).send('Not Found');
-};
-
-const addErrorToRequestLog: ErrorRequestHandler = (err, _req, res, next) => {
-  res.locals.err = err;
-  next(err);
-};
-
-export default (): [RequestHandler, ErrorRequestHandler] => [
-  unexpectedRequest,
-  addErrorToRequestLog,
-];
+export class ErrorHandler {
+  static execute(_req: Request, res: Response) {
+    res.status(404).send();
+  }
+}
