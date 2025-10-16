29a9411886badc5c0e7b7f7fc4d3c1c6c2411c06
joaov1ct0r <jov9051@outlook.com>
2025-10-16 04:59:00 -0300

    feat: added common factories to common folder

---
+++ src/common/factories/repositories/index.ts
@@ -0,0 +1,5 @@
+import { FindUserByEmailRepository } from '@users/repositories/find-user-by-email-repository';
+
+const findUserByEmailRepository = new FindUserByEmailRepository();
+
+export { findUserByEmailRepository };
