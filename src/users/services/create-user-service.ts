e0aeebf8ce74374c115beb660e8a22dc869dad40
joaov1ct0r <jov9051@outlook.com>
2025-10-15 21:36:30 -0300

    feat: created create user service file

---
+++ src/users/services/create-user-service.ts
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt';
+import { env } from '@utils/env-config';
+import { IUserDTO } from '@users/dtos/user-dto';
+import { BaseService } from '@services/base-service';
+import { UserMapper } from '@users/mappers/user-mapper';
+import { ICreateUserDTO } from '@users/dtos/create-user-dto';
+import { IFindUserByEmailRepository } from '@users/repositories/find-user-by-email-repository';
+import { ICreateUserRepository } from '@users/repositories/create-user-repository';
+
+export interface ICreateUserService {
+  execute(userDTO: ICreateUserDTO): Promise<IUserDTO>;
+}
+
+export class CreateUserService
+  extends BaseService
+  implements ICreateUserService
+{
+  private findUserByEmailRepository: IFindUserByEmailRepository;
+  private createUserRepository: ICreateUserRepository;
+
+  constructor(
+    findUserByEmailRepository: IFindUserByEmailRepository,
+    createUserRepository: ICreateUserRepository,
+  ) {
+    super();
+    this.findUserByEmailRepository = findUserByEmailRepository;
+    this.createUserRepository = createUserRepository;
+  }
+
+  public async execute(userDTO: ICreateUserDTO): Promise<IUserDTO> {
+    const isEmailRegistered = await this.findUserByEmailRepository.execute(
+      userDTO.email,
+    );
+
+    if (isEmailRegistered !== null) {
+      this.badRequest('Email não disponível!');
+    }
+
+    userDTO.password = bcrypt.hashSync(userDTO.password, env.HASH_SALT);
+
+    const user = await this.createUserRepository.execute(userDTO);
+
+    return UserMapper.execute(user);
+  }
+}
