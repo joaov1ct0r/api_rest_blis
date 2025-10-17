ab45a23d6d3405aa94f272328cf4e468bc569694
joaov1ct0r <jov9051@outlook.com>
2025-10-17 03:24:04 -0300

    feat: created middleware authorization

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,25 +1,31 @@
-import { BaseController } from '@src/controllers/base-controller';
-import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { env } from '@utils/env-config';
+import { NextFunction, Request, Response } from 'express';
+import { UnauthorizedError } from '@src/errors/unauthorized-error';
+import { ForbiddenError } from '@src/errors/forbidden-error';
 
-export class Authorization extends BaseController {
-  execute(req: Request, res: Response, next: NextFunction) {
+class Authorization {
+  public async execute(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-      throw this.unauthorized('Não autorizado!');
+      throw new UnauthorizedError('Não autorizado!');
     }
 
     const token = authHeader.split(' ')[1];
 
     jwt.verify(token, env.JWT_SECRET, (error, payload) => {
       if (error) {
-        throw this.forbidden('Acesso negado!');
+        throw new ForbiddenError('Acesso negado!');
       }
 
-      req.payload = payload as { userId: string };
+      const { userId } = payload as { userId: string };
+      req.userId = userId;
       next();
     });
   }
 }
+
+const authorization = new Authorization();
+
+export { authorization, Authorization };
