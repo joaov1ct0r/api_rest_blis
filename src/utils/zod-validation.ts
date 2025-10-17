21bec759f8642b3793766812de03d379273c36e3
joaov1ct0r <jov9051@outlook.com>
2025-10-17 03:23:43 -0300

    feat: create zod validation file

--- src/utils/zod-validation.ts
+++ src/utils/zod-validation.ts
@@ -1,16 +1,39 @@
-import { ZodObject } from 'zod';
+import { ZodAny, ZodObject } from 'zod';
 import { NextFunction, Request, Response } from 'express';
 import { BaseController } from '@controllers/base-controller';
 import { authUserBodySchema } from '@auth/schemas/auth-user-body-schema';
 import { createUserBodySchema } from '@users/schemas/create-user-body-schema';
+import { validateFile } from '@documents/schemas/create-user-documents-body-schema';
 
 export class ZodValidation extends BaseController {
   static createUser(req: Request, res: Response, next: NextFunction) {
     ZodValidation.execute(req, res, next, createUserBodySchema);
   }
 
+  static createUserDocuments(req: Request, res: Response, next: NextFunction) {
+    ZodValidation.executeFile(req, res, next, validateFile);
+  }
+
   static authUser(req: Request, res: Response, next: NextFunction) {
     ZodValidation.execute(req, res, next, authUserBodySchema);
+  }
+
+  static executeFile(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    schema: ZodAny,
+  ) {
+    const result = schema.safeParse(req.file);
+
+    if (result.success === false) {
+      return res.status(400).json({
+        error: result.error.format(),
+        status: 400,
+      });
+    }
+
+    next();
   }
 
   static execute(
