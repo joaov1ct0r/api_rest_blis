e21dfc62d9298743b9f6c5e1b3e16c9a718d1653
joaov1ct0r <jov9051@outlook.com>
2025-10-17 21:14:09 -0300

    feat: added jwt validation to abilities router

--- src/abilities/routes/ability-routes.ts
+++ src/abilities/routes/ability-routes.ts
@@ -1,11 +1,13 @@
 import express, { Router } from 'express';
 import { ZodValidation } from '@utils/zod-validation';
+import { authorization } from '@middlewares/authorization';
 import { createAbilityController } from '@abilities/factories/index';
 
 export const abilitiesRouter: Router = express.Router();
 
 abilitiesRouter.post(
   '/',
+  authorization.execute,
   ZodValidation.createAbility,
   createAbilityController.execute.bind(createAbilityController),
 );
