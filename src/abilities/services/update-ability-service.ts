d43669e85af6a8e0cf4b88bc6570d7becf9f7c0d
joaov1ct0r <jov9051@outlook.com>
2025-10-18 01:41:58 -0300

    feat: created update ability service

---
+++ src/abilities/services/update-ability-service.ts
@@ -0,0 +1,30 @@
+import { BaseService } from '@src/services/base-service';
+import { IAbilityDTO } from '@abilities/dtos/ability-dto';
+import { AbilityMapper } from '@abilities/mappers/ability-mapper';
+import { IUpdateAbilityDTO } from '@abilities/dtos/update-ability-dto';
+import { IUpdateAbilityRepository } from '@abilities/repositories/update-ability-repository';
+
+export interface IUpdateAbilityService {
+  execute(dto: IUpdateAbilityDTO, id: string): Promise<IAbilityDTO>;
+}
+
+export class UpdateAbilityService
+  extends BaseService
+  implements IUpdateAbilityService
+{
+  private updateAbilityRepository: IUpdateAbilityRepository;
+
+  constructor(updateAbilityRepository: IUpdateAbilityRepository) {
+    super();
+    this.updateAbilityRepository = updateAbilityRepository;
+  }
+
+  public async execute(
+    dto: IUpdateAbilityDTO,
+    id: string,
+  ): Promise<IAbilityDTO> {
+    const updatedAbility = await this.updateAbilityRepository.execute(dto, id);
+
+    return AbilityMapper.execute(updatedAbility);
+  }
+}
