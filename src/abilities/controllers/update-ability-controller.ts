41401a2026ffe1c1bbd9f919b52602f599365ae4
joaov1ct0r <jov9051@outlook.com>
2025-10-18 01:50:41 -0300

    feat: created controller to route update ability

---
+++ src/abilities/controllers/update-ability-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { BaseController } from '@src/controllers/base-controller';
+import { IUpdateAbilityService } from '@abilities/services/update-ability-service';
+
+export class UpdateAbilityController extends BaseController {
+  private updateAbilityService: IUpdateAbilityService;
+
+  constructor(updateAbilityService: IUpdateAbilityService) {
+    super();
+    this.updateAbilityService = updateAbilityService;
+  }
+
+  public async execute(req: Request, res: Response) {
+    await this.updateAbilityService.execute(req.body, req.userId!);
+
+    return res.status(204).send();
+  }
+}
