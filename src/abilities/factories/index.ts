a3fc14d48a73e4209228cc96ba284f2eda44a224
joaov1ct0r <jov9051@outlook.com>
2025-10-18 01:53:09 -0300

    feat: created singleton for controller to router update abilities

--- src/abilities/factories/index.ts
+++ src/abilities/factories/index.ts
@@ -1,6 +1,9 @@
 import { CreateAbilityService } from '@abilities/services/create-ability-service';
 import { CreateAbilityController } from '@abilities/controllers/create-ability-controller';
 import { CreateAbilityRepository } from '@abilities/repositories/create-ability-repository';
+import { UpdateAbilityRepository } from '../repositories/update-ability-repository';
+import { UpdateAbilityService } from '../services/update-ability-service';
+import { UpdateAbilityController } from '../controllers/update-ability-controller';
 
 const createAbilityRepository = new CreateAbilityRepository();
 
@@ -10,4 +13,12 @@ const createAbilityController = new CreateAbilityController(
   createAbilityService,
 );
 
-export { createAbilityController };
+const updateAbilityRepository = new UpdateAbilityRepository();
+
+const updateAbilityService = new UpdateAbilityService(updateAbilityRepository);
+
+const updateAbilityController = new UpdateAbilityController(
+  updateAbilityService,
+);
+
+export { createAbilityController, updateAbilityController };
