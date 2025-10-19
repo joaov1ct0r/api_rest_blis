30c14f224963a538c20484c48d3848646b71df09
joaov1ct0r <jov9051@outlook.com>
2025-10-19 20:18:13 -0300

    feat: added delete users abilities to factories

--- src/users-abilities/factories/index.ts
+++ src/users-abilities/factories/index.ts
@@ -2,16 +2,32 @@ import { findAbilityByIdRepository } from '@abilities/factories/index';
 import { CreateUsersAbilitiesService } from '@users-abilities/services/create-users-abilities-service';
 import { CreateUsersAbilitiesController } from '@users-abilities/controllers/create-users-abilities-controller';
 import { CreateUsersAbilitiesRepository } from '@users-abilities/repositories/create-users-abilities-repository';
+import { FindUsersAbilitiesByIdRepository } from '@users-abilities/repositories/find-users-abilities-by-id-repository';
+import { DeleteUsersAbilitiesByIdRepository } from '@users-abilities/repositories/delete-users-abilities-by-id-repository';
+import { DeleteUsersAbilitiesService } from '@users-abilities/services/delete-users-abilities-service';
+import { DeleteUsersAbilitiesController } from '../controllers/delete-users-abilities-controller';
 
 const createUsersAbilitiesRepository = new CreateUsersAbilitiesRepository();
+const findUsersAbilitiesByIdRepository = new FindUsersAbilitiesByIdRepository();
+const deleteUsersAbilitiesByIdRepository =
+  new DeleteUsersAbilitiesByIdRepository();
 
 const createUsersAbilitiesService = new CreateUsersAbilitiesService(
   findAbilityByIdRepository,
   createUsersAbilitiesRepository,
 );
 
+const deleteUsersAbilitiesService = new DeleteUsersAbilitiesService(
+  findUsersAbilitiesByIdRepository,
+  deleteUsersAbilitiesByIdRepository,
+);
+
 const createUsersAbilitiesController = new CreateUsersAbilitiesController(
   createUsersAbilitiesService,
 );
 
-export { createUsersAbilitiesController };
+const deleteUsersAbilitiesController = new DeleteUsersAbilitiesController(
+  deleteUsersAbilitiesService,
+);
+
+export { createUsersAbilitiesController, deleteUsersAbilitiesController };
