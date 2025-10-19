db9896ae70ec252aec53a92856aa16b64407daf9
joaov1ct0r <jov9051@outlook.com>
2025-10-19 19:58:35 -0300

    fix: renamed file to delete users abilities by id repository

--- src/users-abilities/repositories/delete-users-abilities-by-id-repository.ts
+++ src/users-abilities/repositories/delete-users-abilities-by-id-repository.ts
@@ -1,13 +1,13 @@
 import { UsersAbilities } from 'generated/prisma';
 import { PrismaProvider } from '@utils/prisma-provider';
 
-export interface IDeleteUsersAbilitiesRepository {
+export interface IDeleteUsersAbilitiesByIdRepository {
   execute(id: string): Promise<UsersAbilities>;
 }
 
-export class DeleteUsersAbilitiesRepository
+export class DeleteUsersAbilitiesByIdRepository
   extends PrismaProvider
-  implements IDeleteUsersAbilitiesRepository
+  implements IDeleteUsersAbilitiesByIdRepository
 {
   public async execute(id: string): Promise<UsersAbilities> {
     const deletedUsersAbilities = await this.prisma.usersAbilities.delete({
