52ec7948decc48e6e389490e9adaa81f4ed98c62
joaov1ct0r <jov9051@outlook.com>
2025-10-20 01:00:39 -0300

    fix: updated user mapper

--- src/users/mappers/user-mapper.ts
+++ src/users/mappers/user-mapper.ts
@@ -2,22 +2,8 @@ import { Users } from 'generated/prisma';
 import { IUserDTO, UserDTO } from '@users/dtos/user-dto';
 
 export class UserMapper {
-  static execute({
-    id,
-    name,
-    birthdate,
-    email,
-    createdAt,
-    updatedAt,
-  }: Users): IUserDTO {
-    const userDTO = new UserDTO({
-      id,
-      name,
-      birthdate: birthdate.toISOString(),
-      email,
-      created_at: createdAt.toISOString(),
-      updated_at: updatedAt.toISOString(),
-    });
+  static execute(entity: Users): IUserDTO {
+    const userDTO = new UserDTO(entity);
 
     return userDTO;
   }
