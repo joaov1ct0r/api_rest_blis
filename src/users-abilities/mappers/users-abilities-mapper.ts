5f55cb1ec23f2bb98e1dc8f83811b0e38892977b
joaov1ct0r <jov9051@outlook.com>
2025-10-20 00:28:24 -0300

    feat: created mapper to many users abilities

--- src/users-abilities/mappers/users-abilities-mapper.ts
+++ src/users-abilities/mappers/users-abilities-mapper.ts
@@ -1,8 +1,16 @@
 import { UsersAbilities } from 'generated/prisma';
 import { UsersAbilitiesDTO } from '@users-abilities/dtos/users-abilities-dto';
+import {
+  IUsersAbilitiesManyDTO,
+  UsersAbilitiesManyDTO,
+} from '@users-abilities/dtos/users-abilities-many-dto';
 
 export class UsersAbilitiesMapper {
   static execute(entity: UsersAbilities): UsersAbilitiesDTO {
     return new UsersAbilitiesDTO(entity);
+  }
+
+  static manyExecute(entity: IUsersAbilitiesManyDTO): UsersAbilitiesManyDTO {
+    return new UsersAbilitiesManyDTO(entity);
   }
 }
