dabd98af845922277890adfa9f7c98e001621cc2
joaov1ct0r <jov9051@outlook.com>
2025-10-20 01:01:35 -0300

    feat: created service for get users abilities

---
+++ src/users-abilities/services/get-users-abilities-service.ts
@@ -0,0 +1,47 @@
+import { IUsersAbilitiesManyDTO } from '@users-abilities/dtos/users-abilities-many-dto';
+import { IGetUsersAbilitiesByUserIdDTO } from '@users-abilities/dtos/get-users-abilities-by-user-id-dto';
+import { BaseService } from '@src/services/base-service';
+import { IGetUsersAbilitiesByUserIdRepository } from '../repositories/get-users-abilities-by-user-id-repository';
+import { UsersAbilitiesMapper } from '../mappers/users-abilities-mapper';
+
+export interface IGetUsersAbilitiesService {
+  execute(
+    dto: IGetUsersAbilitiesByUserIdDTO,
+  ): Promise<IUsersAbilitiesManyDTO[]>;
+}
+
+export class GetUsersAbilitiesService
+  extends BaseService
+  implements IGetUsersAbilitiesService
+{
+  private getUsersAbilitiesByUserIdRepository: IGetUsersAbilitiesByUserIdRepository;
+
+  constructor(
+    getUsersAbilitiesByUserIdRepository: IGetUsersAbilitiesByUserIdRepository,
+  ) {
+    super();
+    this.getUsersAbilitiesByUserIdRepository =
+      getUsersAbilitiesByUserIdRepository;
+  }
+
+  public async execute({
+    user_id,
+    pageSize = 20,
+    skipAmount = 0,
+    pageNumber = 1,
+  }: IGetUsersAbilitiesByUserIdDTO): Promise<IUsersAbilitiesManyDTO[]> {
+    const usersAbilities =
+      await this.getUsersAbilitiesByUserIdRepository.execute({
+        user_id,
+        skipAmount: skipAmount ? skipAmount : (pageNumber - 1) * pageSize,
+        pageSize,
+        pageNumber,
+      });
+
+    const usersAbilitiesManyDTO = usersAbilities.map((usersAbilities) =>
+      UsersAbilitiesMapper.manyExecute(usersAbilities),
+    );
+
+    return usersAbilitiesManyDTO;
+  }
+}
