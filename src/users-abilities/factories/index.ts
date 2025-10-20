1f4c4131ebf4d123de4acd0c178dca9c064a6d9c
joaov1ct0r <jov9051@outlook.com>
2025-10-20 01:12:46 -0300

    feat: created singleton for get users abilities controller

--- src/users-abilities/factories/index.ts
+++ src/users-abilities/factories/index.ts
@@ -5,29 +5,42 @@ import { CreateUsersAbilitiesRepository } from '@users-abilities/repositories/cr
 import { FindUsersAbilitiesByIdRepository } from '@users-abilities/repositories/find-users-abilities-by-id-repository';
 import { DeleteUsersAbilitiesByIdRepository } from '@users-abilities/repositories/delete-users-abilities-by-id-repository';
 import { DeleteUsersAbilitiesService } from '@users-abilities/services/delete-users-abilities-service';
-import { DeleteUsersAbilitiesController } from '../controllers/delete-users-abilities-controller';
+import { DeleteUsersAbilitiesController } from '@users-abilities/controllers/delete-users-abilities-controller';
+import { GetUsersAbilitiesByUserIdRepository } from '@users-abilities/repositories/get-users-abilities-by-user-id-repository';
+import { GetUsersAbilitiesService } from '@users-abilities/services/get-users-abilities-service';
+import { GetUsersAbilitiesController } from '@users-abilities/controllers/get-users-abilities-controller';
 
 const createUsersAbilitiesRepository = new CreateUsersAbilitiesRepository();
 const findUsersAbilitiesByIdRepository = new FindUsersAbilitiesByIdRepository();
 const deleteUsersAbilitiesByIdRepository =
   new DeleteUsersAbilitiesByIdRepository();
+const getUsersAbilitiesByUserIdRepository =
+  new GetUsersAbilitiesByUserIdRepository();
 
 const createUsersAbilitiesService = new CreateUsersAbilitiesService(
   findAbilityByIdRepository,
   createUsersAbilitiesRepository,
 );
-
 const deleteUsersAbilitiesService = new DeleteUsersAbilitiesService(
   findUsersAbilitiesByIdRepository,
   deleteUsersAbilitiesByIdRepository,
+);
+const getUsersAbilitiesService = new GetUsersAbilitiesService(
+  getUsersAbilitiesByUserIdRepository,
 );
 
 const createUsersAbilitiesController = new CreateUsersAbilitiesController(
   createUsersAbilitiesService,
 );
-
 const deleteUsersAbilitiesController = new DeleteUsersAbilitiesController(
   deleteUsersAbilitiesService,
 );
+const getUsersAbilitiesController = new GetUsersAbilitiesController(
+  getUsersAbilitiesService,
+);
 
-export { createUsersAbilitiesController, deleteUsersAbilitiesController };
+export {
+  createUsersAbilitiesController,
+  deleteUsersAbilitiesController,
+  getUsersAbilitiesController,
+};
