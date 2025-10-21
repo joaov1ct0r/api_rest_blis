ed72d44e46e7b81f3f97959cd98597f4b22ce3a9
joaov1ct0r <jov9051@outlook.com>
2025-10-20 22:50:38 -0300

    chore: added retries to file start.sh

--- src/utils/prisma-provider.ts
+++ src/utils/prisma-provider.ts
@@ -1,20 +1,45 @@
+import path from 'node:path';
+import { mkdirSync, existsSync } from 'node:fs';
 import { PrismaClient } from 'generated/prisma';
 
 export class PrismaProvider {
   protected readonly prisma: PrismaClient;
+  private readonly mysql_data_path: string = path.resolve(
+    __dirname,
+    '..',
+    '..',
+    'mysql_data',
+  );
 
   constructor() {
+    if (!existsSync) {
+      mkdirSync(this.mysql_data_path);
+    }
+
     this.prisma = new PrismaClient();
 
-    this.main().catch(async (e) => {
-      console.error(e);
-      await this.prisma.$disconnect();
-      process.exit(1);
-    });
+    (async () => {
+      await this.connectWithRetry();
+    })();
   }
 
-  private async main() {
-    this.prisma.$connect();
+  private async connectWithRetry(retries = 5, delay = 3000): Promise<void> {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+      try {
+        await this.prisma.$connect();
+        console.log('✅ Connected to database');
+        return;
+      } catch (err) {
+        console.error(
+          `❌ Prisma connection failed (attempt ${attempt}/${retries})`,
+        );
+        if (attempt === retries) {
+          console.error('🚨 All retry attempts failed');
+          throw err;
+        }
+        await new Promise((res) => setTimeout(res, delay));
+      }
+    }
   }
 }
 
--- start.sh
+++ start.sh
@@ -9,9 +9,19 @@ echo "Starting new containers..."
 docker compose up -d
 echo "OK!"
 
-echo "Checking for missing migrations..."
-npx prisma migrate dev
-echo "OK!"
+echo "Waiting for MySQL to be ready..."
+until docker exec db_blis mysqladmin ping -h"localhost" --silent; do
+  sleep 2
+done
+echo "MySQL ready!"
+
+retries=5
+until npx prisma migrate dev || [ $retries -le 0 ]; do
+  echo "Prisma migrate failed, retrying..."
+  retries=$((retries - 1))
+  sleep 3
+done
+
 
 echo "Starting API..."
 npx ts-node-dev --transpile-only -r tsconfig-paths/register src/index.ts
